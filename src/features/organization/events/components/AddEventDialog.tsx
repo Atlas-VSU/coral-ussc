b8fe0f41a1aac7ef43304f1f196af10fe62e76ca
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2026-02-22 12:13:42 +0800

    fix: ensures add event modal is scrollable

--- src/features/organization/events/components/AddEventDialog.tsx
+++ src/features/organization/events/components/AddEventDialog.tsx
@@ -89,7 +89,7 @@ export function AddEventDialog({
 
   return (
     <Dialog open={open} onOpenChange={loading ? undefined : onOpenChange}>
-      <DialogContent className="sm:max-w-[500px] h-auto overflow-hidden pt-12 pb-4">
+      <DialogContent className="sm:max-w-[500px] h-[calc(100vh-10rem)] pt-12 pb-4 overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Add New Event</DialogTitle>
           <DialogDescription>
