60d587266d8ba03aa8138155d9b241a24c75ec71
Jaballa, Jade A. <jade.abadiano.jaballa@gmail.com>
2026-03-05 21:04:06 +0800

    Added constants for fines and payment types and status

---
+++ src/constants/status.ts
@@ -0,0 +1,13 @@
+export enum PaymentStatus {
+  PENDING = "pending",
+  VERIFIED = "verified",
+  REJECTED = "rejected",
+}
+
+export enum FineStatus {
+    UNPAID = "unpaid",
+    PARTIAL = "partially_paid",
+    PAID = "paid",
+    PENDING = "pending",
+    WAIVED = "waived",
+    }
---
+++ src/constants/types.ts
@@ -0,0 +1,4 @@
+export enum PaymentType {
+  MEMBERSHIP = "membershipFees",
+  FINES = "fines",
+}
