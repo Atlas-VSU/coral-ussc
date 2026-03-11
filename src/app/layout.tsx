866249d68d6b4fff21bca60b1aa414c95a8d1c51
Pauline Dejos <22-1-00454@vsu.edu.ph>
2026-03-12 04:07:28 +0800

    feat: setup for major frontend changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 "use client";
-import { Nunito, Nunito_Sans } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-const nunito = Nunito({
-  variable: "--font-nunito",
-  subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-const nunitoSans = Nunito_Sans({
-  variable: "--font-nunito-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
@@ -38,7 +32,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#3b82f6" />
       </head>
       <body
-        className={`${nunito.variable} ${nunitoSans.variable} antialiased`}
+        className={`${montserrat.variable} antialiased`}
         suppressHydrationWarning
       >
         <div suppressHydrationWarning style={{ display: 'contents' }}>
