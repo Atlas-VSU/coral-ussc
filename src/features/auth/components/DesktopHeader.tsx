4a2a1ccf816d84d643c3ccbe751683af0249ed0f
Shn Kerby Dolamos <23-1-02186@vsu.edu.ph>
2026-03-14 16:41:29 +0800

    feat (homepage): Import USSC Connect homepage design to homepage
    - Refactor and change design of homepage to match ussc connect

---
+++ src/features/auth/components/DesktopHeader.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+
+export function DesktopHeader() {
+  return (
+    <div>
+      <header className="absolute m-8 inset-3 top-0 z-1 px-6 sm:px-10 py-0 flex items-center justify-between bg-transparent h-20">
+        <div className="inline-flex items-center gap-4 group">
+          <img
+            src="/images/main-banner-2.png"
+            alt="USSC-Connect"
+            className="ml-5 h-20 w-auto  object-contain"
+          />
+        </div>
+        <nav className="hidden md:flex items-center gap-8 text-sm font-regular text-[#2E7D32]">
+          <Link
+            href="/"
+            className="hover:text-[#2E7D32] transition-colors uppercase"
+          >
+            Home
+          </Link>
+
+          <Link
+            href="/about"
+            className="hover:text-[#2E7D32] transition-colors uppercase"
+          >
+            About
+          </Link>
+
+          <Link
+            href="/contact"
+            className="hover:text-[#2E7D32] transition-colors uppercase"
+          >
+            Contact
+          </Link>
+
+          <Link
+            href="/login"
+            className="px-4 py-2 rounded-lg bg-linear-to-r from-[#8BC34A] to-[#2E7D32] text-white hover:bg-[#2E7D32] transition-colors uppercase"
+          >
+            Login
+          </Link>
+        </nav>
+      </header>
+    </div>
+  );
+}
---
+++ src/features/auth/components/MobileHeader.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export function MobileHeader() {
+  return (
+    <div>
+      <header className="absolute inset-5 m-2 top-0 z-30 px-1 sm:px-6 py-0 flex items-center justify-between bg-white h-16">
+        <img
+          src="/images/main-banner-2.png"
+          alt="USSC-Connect"
+          className="h-12 w-auto object-contain"
+        />
+        <Link
+          href="/login"
+          className="px-3 py-1.5 rounded-lg bg-linear-to-r from-[#8BC34A] to-[#2E7D32] text-white hover:bg-[#2E7D32] text-xs font-semibold transition-colors uppercase"
+        >
+          Login
+        </Link>
+      </header>
+    </div>
+  );
+}
