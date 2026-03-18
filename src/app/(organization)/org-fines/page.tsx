62cfb864d11d1984e20f6b09073176fe14cd6cde
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-03-19 05:25:02 +0800

    Improve Fines padding

--- src/app/(organization)/org-fines/page.tsx
+++ src/app/(organization)/org-fines/page.tsx
@@ -17,7 +17,7 @@ import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-
+// import { StatCard } from "@/features/organization/fees/local-components/StatCard";
 import { useState, useEffect, useCallback, useRef, useMemo } from "react";
 import { countFinesOfStudents, countStudentsWithFines, countUnsettleFinesOfStudents, getAllFines } from "@/firebase/fines/read/fines";
 import { FineBreakdownDialog } from "@/features/organization/fines/components/FineBreakdownDialog";
@@ -95,8 +95,8 @@ export default function FinesPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col gap-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div className="min-h-screen">
+      <div className="flex flex-col gap-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
 
         <FinesHeader
           onAddFineType={handleAddFineType}
