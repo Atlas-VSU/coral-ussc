71564c79c28ef0fa6e5cb32be14364c87400ef25
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-03-18 21:09:55 +0800

    Move local-components to features/organization/fees

--- src/app/(organization)/org-fees/page.tsx
+++ src/app/(organization)/org-fees/page.tsx
@@ -1,6 +1,6 @@
 // import { FeesPage } /from "@/features/organization/fees/components/FeesPage";
 
-import { FeesPage } from "./local-components/FeesPage";
+import { FeesPage } from "@/features/organization/fees/local-components/FeesPage";
 export default function Page() {
     return (<>
         <FeesPage/>
