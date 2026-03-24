fc3d8cb76924dc422b4313cf736d38d79d3a5b53
Jaballa, Jade A. <jade.abadiano.jaballa@gmail.com>
2026-03-24 18:10:40 +0800

    Fix: Ensured a unique receipt code for every transaction using time and string conversion

--- src/features/organization/payments/utils.ts
+++ src/features/organization/payments/utils.ts
@@ -1,11 +1,13 @@
-
 import { customAlphabet } from 'nanoid';
-const cleanAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+const cleanAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const generateCleanId = customAlphabet(cleanAlphabet, 4);
 
-
 export const generateReceiptId = () => {
-  const timestamp = Date.now().toString();
-  const receiptId = `USSC-${generateCleanId()}-${timestamp.slice(-4)}`;
-  return receiptId;
+
+  const timeComponent = Math.floor(Date.now() / 1000).toString(36).toUpperCase();
+
+  const randomComponent = generateCleanId();
+  
+  return `USSC-${timeComponent}-${randomComponent}`;
 }
